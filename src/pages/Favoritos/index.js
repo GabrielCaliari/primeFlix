fd399dc4dd193d505fbbd62cbd20c97c0756e77e
Gabriel Caliari <gabriel15caliari@hotmail.com>
2024-08-23 14:57:21 -0300

    adicionando a funcao de remover filme do favorito

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+import "./favoritos.css"
+
+function Favoritos () {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+
+    const minhaLista = localStorage.getItem("@primeflix");
+    setFilmes(JSON.parse(minhaLista) || []);
+
+  }, [])
+
+  function excluirFilme(id){
+      const filtroFilmes = filmes.filter((item) => {
+          return (item.id !== id);
+      })
+
+      setFilmes(filtroFilmes);
+      localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+  }
+
+  return(
+    <div className="meus-filmes">
+      <h1>Meus filmes</h1>
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo...</span>}
+      <ul>
+        {filmes.map((item) => {
+          return (
+            <li key={item.id}>
+              <span>{item.title}</span>
+
+              <div>
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                <button onClick={() => excluirFilme(item.id)} >Excluir</button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Favoritos
