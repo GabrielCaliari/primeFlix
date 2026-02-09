de722c7faee0445398e3512436e80fa76c1834bc
Gabriel Caliari <gabriel15caliari@hotmail.com>
2026-02-09 11:24:56 -0300

    retirando titulo do header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,10 +4,6 @@ import ListFilmes from "../ListFilmes";
 function Home() {
   return (
     <div className="home">
-      <section className="home-hero">
-        <h1>Bem-vindo ao Prime Flix</h1>
-        <p>Descubra filmes incríveis e organize sua lista favorita.</p>
-      </section>
       <ListFilmes title="Melhor avaliação" fetchUrl="movie/top_rated" />
       <ListFilmes title="Em cartaz" fetchUrl="/movie/now_playing" />
       <ListFilmes title="Populares" fetchUrl="movie/popular" />
